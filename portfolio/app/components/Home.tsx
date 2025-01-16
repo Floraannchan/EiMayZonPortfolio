26e0a352014434b7f154acc07d64b571664dfe30
Ei May Zon <eimayzon@Eis-MacBook-Air.local>
2025-01-16 22:07:44 +0700

    before change home file

--- portfolio/app/components/Home.tsx
+++ portfolio/app/components/Home.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+// import { useScroll } from "motion/react";
 import AboutMe from "./AboutMe";
+import Education from "./Education";
+import Hero from "./Hero";
+// import { useRef } from "react";
 
 export default function Home() {
   return (
     <main className="relative h-[100vh]">
+      <Hero />
       <AboutMe />
+      <Education />
     </main>
   );
 }
